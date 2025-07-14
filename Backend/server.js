003931f7b3c14f61613456ec7d42d28e7f58d2bb
sahithdaddla <daddlasahith>
2025-07-14 15:50:34 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -36,7 +36,7 @@ const pool = new Pool({
 // Middleware
 app.use(cors());
 app.use(cors({
-    origin: ['http://16.170.253.18:3106', 'http://127.0.0.1:5501'],
+    origin: ['http://13.51.205.49:3106', 'http://127.0.0.1:5501'],
     methods: ['GET', 'POST', 'OPTIONS'],
     allowedHeaders: ['Content-Type']
 }));
@@ -266,9 +266,9 @@ pool.connect()
         initializeDatabase().then(() => {
            app.listen(port, '0.0.0.0', () => {
                 logger.info(`Server running on port ${port}`);
-                console.log(`Health check: http://16.170.253.18:${port}/health`);
-                console.log(`HR Dashboard: http://16.170.253.18:${port}/attendance.html`);
-                console.log(`Employee Asset Request: http://16.170.253.18:${port}/employee.html`);
+                console.log(`Health check: http://13.51.205.49:${port}/health`);
+                console.log(`HR Dashboard: http://13.51.205.49:${port}/attendance.html`);
+                console.log(`Employee Asset Request: http://13.51.205.49:${port}/employee.html`);
             });
         });
     })
